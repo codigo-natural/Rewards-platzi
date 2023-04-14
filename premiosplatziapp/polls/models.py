00555ccb5469935cc2c6665c6ef09ae0bc1c53d1
Code-Nature <codenature01001@gmail.com>
2023-04-14 18:40:56 -0500

    se solucionaron algunos bugs en la creacion de tablas para la base de datos

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Question(models.Model):
-  question_textm = models.CharField(max_length=200)
+  question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
-class Choices(models.Model):
+class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
