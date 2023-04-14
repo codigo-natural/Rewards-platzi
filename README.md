6e65fcd8228c610b163ebe808f5cf73f900c3e38
Code-Nature <codenature01001@gmail.com>
2023-04-14 17:26:07 -0500

    se creo el diagrama entidad-relacion del proyecto

--- README.md
+++ README.md
@@ -4,4 +4,47 @@ Estructura de carpetas de un proyecto con Django
 [Estructura de carpetas de un proyecto con Django](https://studygyaan.com/wp-content/uploads/2019/07/Best-Practice-to-Structure-Django-Project-Directories-and-Files.png)
 
 - El archivo "manage.py" es el archivo principal que se utiliza para interactuar con el proyecto de Django y realizar tareas como la creación de aplicaciones, la ejecución de pruebas y la administración de la base de datos.
-- 
+
+> diagrama entidad-relacion del proyecto
+# Espacio Trabajo
+
+## Pag. Tools Web
+
+---
+
+[Algoritmos](https://www.notion.so/Algoritmos-4cb88a35678043689b272b7d89d70571)
+
+[Lofi.co - Craft your focus environment](https://app.lofi.co/)
+
+[SAVEE](https://savee.it/)
+
+[OpenAI API](https://beta.openai.com/)
+
+[DALL·E](https://labs.openai.com/)
+
+[React Projects || John Smilga](https://www.notion.so/React-Projects-John-Smilga-61c597bbfb0a4bdcb6daab572c6d3929)
+
+[php Laravel](https://www.notion.so/php-Laravel-11b399a61cd4468d8ce25c2ad30075e5)
+
+[](https://source.unsplash.com/random)
+
+[Snappr Photography | Powering the internet's photos](https://www.snappr.com/)
+
+[pattern.css - Background Patterns in CSS](https://www.notion.so/pattern-css-Background-Patterns-in-CSS-888ea0ae07704f91814d1122f42546f4)
+
+[Plantillas charlas tecnicas](https://www.notion.so/Plantillas-charlas-tecnicas-c8ab9f7b79334256a0e3750cbef2b02f)
+
+**questions**
+
+| id | int |
+| --- | --- |
+| question_text | varchar |
+| pub_date | datetime |
+
+**choices**
+
+| id | int |
+| --- | --- |
+| question | int |
+| choice_text | varchar |
+| votes | int |
--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# Importamos la clase Path de la librería pathlib
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Definimos la variable BASE_DIR como la ruta absoluta al directorio padre del archivo "settings.py"
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -24,33 +25,42 @@ SECRET_KEY = 'django-insecure-(i&)h!#r%ac2*w7!3l-1!ko%37r#q82l4s*^g95)l*a$22)716
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# Lista de hosts permitidos. Vacío significa que cualquier host está permitido.
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# Definición de aplicaciones instaladas en la aplicación
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', # proporciona la interfaz de administración de Django.
+    'django.contrib.auth', # proporciona el sistema de autenticación de usuarios de Django.
+    'django.contrib.contenttypes', # proporciona una forma de asociar contenido (modelos) con permisos.
+    'django.contrib.sessions', # proporciona soporte para el manejo de sesiones de usuario.
+    'django.contrib.messages', # proporciona soporte para mensajes de error en la aplicación.
+    'django.contrib.staticfiles', #  proporciona soporte para la gestión de archivos estáticos (CSS, JavaScript, imágenes) en la aplicación.
 ]
 
+# Definición de middleware utilizado en la aplicación
 MIDDLEWARE = [
+    # proporciona seguridad en la aplicación mediante la configuración de encabezados HTTP.
     'django.middleware.security.SecurityMiddleware',
+    # proporciona soporte para el manejo de sesiones de usuario.
     'django.contrib.sessions.middleware.SessionMiddleware',
+    #  proporciona funcionalidad común de middleware, como la configuración de encabezados HTTP y la redirección de URL.
     'django.middleware.common.CommonMiddleware',
+    # proporciona protección contra ataques CSRF (Cross-Site Request Forgery).
     'django.middleware.csrf.CsrfViewMiddleware',
+    # proporciona soporte para la autenticación de usuarios en la aplicación.
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # proporciona soporte para mensajes de error en la aplicación.
     'django.contrib.messages.middleware.MessageMiddleware',
+    # proporciona protección contra ataques de clickjacking.
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+# Configuración de la URL raíz de la aplicación
 ROOT_URLCONF = 'premiosplatziapp.urls'
 
+# Definición de plantillas para la aplicación
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -66,13 +76,13 @@ TEMPLATES = [
         },
     },
 ]
-
+# Configuración de la aplicación WSGI (interfaz de pasarela de servidor web)
 WSGI_APPLICATION = 'premiosplatziapp.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Configuración de la base de datos
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,6 +94,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+# Configuración de validación de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -99,25 +110,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+# Configuración de internacionalización
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
+# Código de idioma predeterminado
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+# Zona horaria predeterminada
+TIME_ZONE = 'America/Bogota'
+# Habilitar internacionalización
 USE_I18N = True
-
+# Habilitar zonas horarias
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# URL base para los archivos estáticos
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+# Tipo de campo de clave primaria predeterminado
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
