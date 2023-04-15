962437f65c11a808d7d9eb461a0e30d181e95418
Code-Nature <codenature01001@gmail.com>
2023-04-15 09:50:42 -0500

    se crearon las vistas para la aplicacion

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
 from . import views
-
+# Lista de rutas URL que el servidor Django puede manejar
 urlpatterns = [
-    path('', views.index, name='index')
+    # Ruta para la página de inicio, con una vista llamada "index"
+    path("", views.index, name="index"),
+    # Ruta para la vista de detalle de una pregunta, 
+    # con una vista llamada "detail" y un parámetro de int llamado "question_id"
+    path("detail/<int:question_id>", views.detail, name="detail"),
+    # Ruta para la vista de resultados de una pregunta,
+    # con una vista llamada "results" y un parámetro de int llamado "question_id"
+    path("results/<int:question_id>", views.results, name="results"),
+    # Ruta para la vista de votación de una pregunta, 
+    # con una vista llamada "vote" y un parámetro de int llamado "question_id"
+    path("vote/<int:question_id>", views.vote, name="vote"),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('Hello World')
+  return HttpResponse('estas en la página principal de Premios Platzi App ')
+
+def detail(request, question_id):
+  return HttpResponse(f'Estas viendo la pregunta número {question_id}')
+
+def results(request, question_id):
+  return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+
+def vote(request, question_id):
+  return HttpResponse(f'Estas votando a la pregunda número {question_id} {question_id}')
