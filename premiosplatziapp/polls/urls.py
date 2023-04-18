dd24d95805fdc05742fecb950e8a18f6d9ecfe55
Code-Nature <codenature01001@gmail.com>
2023-04-18 06:51:50 -0500

    Implementando generic views en la aplicación

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -6,13 +6,13 @@ app_name="polls"
 # Lista de rutas URL que el servidor Django puede manejar
 urlpatterns = [
     # Ruta para la página de inicio, con una vista llamada "index"
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # Ruta para la vista de detalle de una pregunta, 
     # con una vista llamada "detail" y un parámetro de int llamado "question_id"
-    path("detail/<int:question_id>", views.detail, name="detail"),
+    path("detail/<int:pk>", views.DetailView.as_view(), name="detail"),
     # Ruta para la vista de resultados de una pregunta,
     # con una vista llamada "results" y un parámetro de int llamado "question_id"
-    path("results/<int:question_id>", views.results, name="results"),
+    path("results/<int:pk>", views.ResultView.as_view(), name="results"),
     # Ruta para la vista de votación de una pregunta, 
     # con una vista llamada "vote" y un parámetro de int llamado "question_id"
     path("vote/<int:question_id>", views.vote, name="vote"),
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,33 +2,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 # Importando los modelos de nuestra aplicación "polls"
 from . models import Question, Choice
+class IndexView(generic.ListView):
+  # Definimos el nombre de la plantilla que se utilizará para la vista
+  template_name = 'polls/index.html'
+  # Definimos el nombre del objeto que se utilizará para representar la lista de objetos que se mostrarán en la vista
+  context_object_name = 'latest_question_list'
 
-def index(request):
-  # Obteniendo todas las preguntas de nuestra base de datos
-  latest_question_list = Question.objects.all()
-  # Renderizando la plantilla "polls/index.html" y pasándole como contexto
-  # la lista de preguntas obtenidas anteriormente
-  return render(request, 'polls/index.html', {
-    'latest_question_list': latest_question_list
-  })
-
-def detail(request, question_id):
-  # Obteniendo la pregunta correspondiente al ID proporcionado o devolviendo un error 404 en caso de que no exista
-  question = get_object_or_404(Question, pk=question_id)
-  # Renderizando la plantilla "polls/detail.html" y pasándole como contexto
-  # la pregunta obtenida anteriormente
-  return render(request, 'polls/detail.html', {
-    'question': question
-  })
-
-def results(request, question_id):
-  # Obteniendo la pregunta correspondiente al ID proporcionado o devolviendo un error 404 en caso de que no exista
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/results.html', {
-    'question': question
-  })
+  def get_queryset(self):
+    """Return the last five published questions"""
+    # Selecciona las últimas cinco preguntas y las ordena por fecha de publicación descendente
+    return Question.objects.order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+    # Definimos el modelo que se utilizará para la vista
+    model = Question
+    # Definimos el nombre de la plantilla que se utilizará para la vista
+    template_name = "polls/detail.html"
+class ResultView(DetailView):
+    # Definimos el nombre de la plantilla que se utilizará para la vista
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
   # Obteniendo la pregunta correspondiente al ID proporcionado o devolviendo un error 404 en caso de que no exista
